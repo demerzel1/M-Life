38c408ab89c9074d6ad2f296a366c957f72ca1ae
demerzel <mengzhaoxin@hotmail.com>
2018-04-16 11:17:40 +0800

    add updateMovie

--- src/main/java/com/dhu/model/CinemaEntity.java
+++ src/main/java/com/dhu/model/CinemaEntity.java
@@ -15,6 +15,7 @@ public class CinemaEntity {
     private int cityId;
 
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     public int getId() {
         return id;
